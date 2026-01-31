dbb219d3c7e54b8733ffe7d4504fe61e61fa909e
Arthur Perron <arthur.perron2081@gmail.com>
2026-01-31 15:35:52 -0500

    ajouter Page Plats

--- TP1_Resto/RestoDbContext.cs
+++ TP1_Resto/RestoDbContext.cs
@@ -8,14 +8,16 @@ using TP1_Resto.Models;
 
 namespace TP1_Resto.Views
 {
-    public class RestDbConext
+    public class RestoDbContext : DbContext
     {
+        public RestoDbContext() { }
+
         public DbSet<Restaurant> Restaurants { get; set; }
 
         public DbSet<Plat> Plats { get; set; }
 
         //Change la chaine de connexion pour lancer la bd
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		=> optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DevLogTP4;Trusted_Connection=true;TrustServerCertificate=true;");
+		=> optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TP1_Resto;Trusted_Connection=true;TrustServerCertificate=true;");
 	}
 }
